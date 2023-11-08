c0f49abdfe1208250c578fe90176390fe75b84d3
coreyfsa <coreyfsa@DESKTOP-TCPPHAT.localdomain>
2023-11-08 14:51:09 -0700

    List of artists shows up. Clicking artist name displays name on new page. Back button is tiny and images won't display.

--- practice.js
+++ practice.js
@@ -1,3 +1,18 @@
 const apiBaseURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/`
 
+const state = {
+    allArtists: [],
+}
+//getAllArtists goes to the URL, retrieves the big list of details for artists
+//then adds .json to make it readable
+//then puts the jsonResponse.data into the array state.allArtists
+const getAllArtists = async() => {
+    const response = await fetch (`${apiBaseURL}artists`)
+    const jsonResponse = await response.json();
+    state.allArtists = jsonResponse.data;
+}
+getAllArtists();
 
+const renderAllArtists = () => {
+    
+}
